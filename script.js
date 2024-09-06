4b967bf552e520aa8daf801a25d26d376cb6e245
wise <42545954+imfuckingwise@users.noreply.github.com>
2024-09-06 21:06:40 +0800

    修復一些BUGS

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// 初始化時自動偵測系統主題
+window.onload = function() {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (prefersDarkScheme.matches) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+};
+
+// 切換深色模式
 function toggleDarkMode() {
     const body = document.body;
     body.classList.toggle("dark-mode");
